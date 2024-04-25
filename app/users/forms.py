f3e8e5532e22f37296d3e59959fc2736024c6abe
Popov Stas <boss.staspopov@mail.ru>
2024-04-25 19:34:45 +0300

    create register user form

---
+++ app/users/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+
+class RegisterUserForm(forms.ModelForm):
+    username = forms.CharField(label='Логин')
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput())
+    password2 = forms.CharField(label='Пароль', widget=forms.PasswordInput())
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+        labels = {
+            'email': 'E-mail',
+            'first_name': 'Имя',
+            'last_name': 'Фамилия '
+        }
