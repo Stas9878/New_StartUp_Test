5b645dcf6bacbc08f333fc1b46a774b451068253
Popov Stas <boss.staspopov@mail.ru>
2024-04-25 19:56:29 +0300

    create Login form

--- app/users/forms.py
+++ app/users/forms.py
@@ -2,6 +2,16 @@ from django import forms
 from django.contrib.auth import get_user_model
 
 
+class LoginUserForm(forms.Form):
+    username = forms.CharField(label='Логин', 
+        widget=forms.TextInput(attrs={
+        'class': 'form-input'
+    }))
+    password = forms.CharField(label='Пароль', 
+        widget=forms.PasswordInput(attrs={
+        'class': 'form-input'
+    }))
+
 
 class RegisterUserForm(forms.ModelForm):
     username = forms.CharField(label='Логин')
