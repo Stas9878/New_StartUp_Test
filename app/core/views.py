b270773f374999b7ed3b9ec82d6aad91c6d8766f
Popov Stas <boss.staspopov@mail.ru>
2024-04-25 19:24:52 +0300

    create base.html template

--- app/core/views.py
+++ app/core/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
+
+def index(request):
+    return render(request, 'core/base.html')
 # Create your views here.
