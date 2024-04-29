dd19530036b3d9b9c0aa62edef6bd03592efdeaa
Popov Stas <boss.staspopov@mail.ru>
2024-04-29 10:17:09 +0300

    add auth route

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,6 +21,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('links.urls')),
-    path('users/', include('users.urls')),
+    path('api/v1/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/v1/', include('api.urls')),
+    path('users/', include('users.urls')),
+    
+    
 ]
