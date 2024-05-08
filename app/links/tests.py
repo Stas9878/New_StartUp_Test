b03874818be5fba42147a667af48f5365ad993cc
Popov Stas <boss.staspopov@mail.ru>
2024-05-08 11:30:53 +0300

    add links test

--- app/links/tests.py
+++ app/links/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+class LinksTest(TestCase):
+    '''Тестирование приложения links'''
+
+
+    def test_link_page(self) -> None:
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Главная')
+        self.assertContains(response, 'API')
+    
+    
+    def test_short_url(self):
+        response = self.client.post('/', data={
+            'target_url': 'https://example.com/example/lesson/api/1'
+        })
+        self.assertContains(response, 'Результат:')
+        print(response.content.decode())
+
+
